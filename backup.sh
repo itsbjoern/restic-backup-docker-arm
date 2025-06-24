64753b896d1ebbaa7f0334b7e870408238bb5258
Björn Friedrichs <mail@bjoernf.com>
2025-06-24 22:28:43 +0100

    Use s-nail

--- backup.sh
+++ backup.sh
@@ -79,7 +79,7 @@ if [ -n "${TEAMS_WEBHOOK_URL}" ]; then
 fi
 
 if [ -n "${MAILX_ARGS}" ]; then
-    sh -c "mail -v -S sendwait ${MAILX_ARGS} < ${lastLogfile} > ${lastMailLogfile} 2>&1"
+    sh -c "s-nail -v -S sendwait ${MAILX_ARGS} < ${lastLogfile} > ${lastMailLogfile} 2>&1"
     if [ $? == 0 ]; then
         echo "Mail notification successfully sent."
     else
--- build.sh
+++ build.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-docker build --rm -t bfriedrichs/restic-backup-docker:latest .
+docker buildx build --platform linux/arm64,linux/amd64 --push --tag bfriedrichs/restic-backup-docker:latest --file Dockerfile .
